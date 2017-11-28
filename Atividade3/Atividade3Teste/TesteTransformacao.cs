3dfa19382c46f3e06d70dd3ba9ad802265ca8375
Thiago Soares <thiagomullet@gmail.com>
2017-11-28 21:04:41 -0200

    Primeiro teste

--- Atividade3/Atividade3Teste/TesteTransformacao.cs
+++ Atividade3/Atividade3Teste/TesteTransformacao.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Atividade3;
 
 namespace Atividade3Teste
 {
     [TestClass]
     public class TesteTransformacao
     {
-        [TestMethod]
-        public void Valores1_10()
+        private Form1 _Conversor;
+
+        [TestInitialize]
+        public void Inicializar()
         {
-            Assert.Inconclusive();
+            _Conversor = new Form1();
+        }
+
+        /*
+         * 
+         * 42,00 = Quarenta e dois reais
+678,00 = Seiscentos e setenta e oito reais
+13,92 = Valor inválido
+-23,00 = Valor Inválido
+1456,00 = Valor Inválido
+         * */
+
+        [TestMethod]
+        public void TesteQuarendaEDois()
+        {
+            Assert.AreEqual(_Conversor.ProcessarNumero("42"),"42 - Quarenta e dois reais.");
         }
     }
 }
