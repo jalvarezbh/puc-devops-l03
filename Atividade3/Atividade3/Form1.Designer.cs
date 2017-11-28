41dfbfb434669bfcde1aaa55433d573c39e6bc09
Joao Alvarez <jalvarezbh@gmail.com>
2017-11-28 20:23:49 -0200

    Versão 1 sem tratar os valores quando o decimal é 1

--- Atividade3/Atividade3/Form1.Designer.cs
+++ Atividade3/Atividade3/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.lstValoresTexto = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.Location = new System.Drawing.Point(165, 7);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(30, 23);
+            this.btnAdicionar.TabIndex = 0;
+            this.btnAdicionar.Text = "+";
+            this.btnAdicionar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(59, 7);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(10, 10);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(43, 13);
+            this.lblValor.TabIndex = 2;
+            this.lblValor.Text = "VALOR";
+            // 
+            // lstValoresTexto
+            // 
+            this.lstValoresTexto.FormattingEnabled = true;
+            this.lstValoresTexto.Location = new System.Drawing.Point(13, 45);
+            this.lstValoresTexto.Name = "lstValoresTexto";
+            this.lstValoresTexto.Size = new System.Drawing.Size(182, 95);
+            this.lstValoresTexto.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(221, 169);
+            this.Controls.Add(this.lstValoresTexto);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.btnAdicionar);
+            this.Name = "Form1";
+            this.Text = "Atividade 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.ListBox lstValoresTexto;
     }
 }
 
--- Atividade3/Atividade3/Form1.cs
+++ Atividade3/Atividade3/Form1.cs
@@ -16,5 +16,146 @@ namespace Atividade3
         {
             InitializeComponent();
         }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal valorDecimal = Convert.ToDecimal(txtValor.Text);
+
+                int valorInteiro = Convert.ToInt32(valorDecimal);
+
+                if (valorInteiro <= 0)
+                    throw new Exception();
+
+                lstValoresTexto.Items.Add($"{txtValor.Text} - {ConvertTextual(valorInteiro.ToString())}");
+            }
+            catch (Exception)
+            {
+                lstValoresTexto.Items.Add($"{txtValor.Text} - Valor inválido");
+            }
+            
+        }
+
+        private string ConvertTextual(string numero)
+        {
+            int tamanho = numero.Length;
+            int quadroValor = 0;
+            StringBuilder retorno = new StringBuilder();
+            while (quadroValor != tamanho)
+            {
+                switch (tamanho - quadroValor)
+                {
+                    case 3:
+                        retorno.Append(VerificaCentena(Convert.ToInt16(numero.Substring(quadroValor, 1))));
+                        break;
+                    case 2:
+                        retorno.Append(VerificaDezena(Convert.ToInt16(numero.Substring(quadroValor, 1))));
+                        break;
+                    case 1:
+                        retorno.Append(VerificaUnidade(Convert.ToInt16(numero.Substring(quadroValor, 1))));
+                        break;
+                    default:
+                        retorno.Append("Valor Inválido");
+                        break;
+                }
+
+                retorno.Append(" ");
+                quadroValor++;
+            }
+
+            if (numero.Equals("1"))
+                retorno.Append("real");
+            else
+                retorno.Append("reais");
+
+            var resultado = retorno.ToString();
+            resultado = resultado.First().ToString().ToUpper() + resultado.Substring(1);
+            return resultado;
+
+        }
+
+        private string VerificaUnidade(short unidade)
+        {
+            switch (unidade)
+            {
+                case 1:
+                    return "um";
+                case 2:
+                    return "dois";
+                case 3:
+                    return "três";
+                case 4:
+                    return "quatro";
+                case 5:
+                    return "cinco";
+                case 6:
+                    return "seis";
+                case 7:
+                    return "sete";
+                case 8:
+                    return "oito";
+                case 9:
+                    return "nove";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private string VerificaDezena(short dezena)
+        {
+            switch (dezena)
+            {
+                case 1:
+                    return "dez";
+                case 2:
+                    return "vinte";
+                case 3:
+                    return "trinta";
+                case 4:
+                    return "quarenta";
+                case 5:
+                    return "cinquenta";
+                case 6:
+                    return "sessenta";
+                case 7:
+                    return "setenta";
+                case 8:
+                    return "oitenta";
+                case 9:
+                    return "noventa";
+                default:
+                    return string.Empty;
+            }
+
+        }
+
+        private string VerificaCentena(short centena)
+        {
+            switch (centena)
+            {
+                case 1:
+                    return "cem";
+                case 2:
+                    return "duzentos";
+                case 3:
+                    return "trezentos";
+                case 4:
+                    return "quatrocentos";
+                case 5:
+                    return "quinhentos";
+                case 6:
+                    return "seiscentos";
+                case 7:
+                    return "setecentos";
+                case 8:
+                    return "oitocentos";
+                case 9:
+                    return "novecentos";
+                default:
+                    return string.Empty;
+            }
+
+        }
     }
 }
